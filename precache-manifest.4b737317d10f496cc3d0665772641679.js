055e4ae6526d5f730dd35409cee337540e2ab1d4
Robert-OP <popescu.robert.octavian@gmail.com>
2018-10-29 23:40:09 +0100

    Updates

---
+++ precache-manifest.4b737317d10f496cc3d0665772641679.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "406961343a0a5a52b1cf",
+    "url": "/venue/static/css/main.7596f66a.chunk.css"
+  },
+  {
+    "revision": "406961343a0a5a52b1cf",
+    "url": "/venue/static/js/main.40696134.chunk.js"
+  },
+  {
+    "revision": "06336d0d2056353705b0",
+    "url": "/venue/static/js/1.06336d0d.chunk.js"
+  },
+  {
+    "revision": "a2c9cb2b5d2d2e65ac63",
+    "url": "/venue/static/js/runtime~main.a2c9cb2b.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/venue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/venue/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/venue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "e19af738b6fb391f1d16d1272a840466",
+    "url": "/venue/index.html"
+  }
+];
